c6a7447c7b5d0b6b9cd823697e164d4b73138301
j-wood105 <jb@jbwood.io>
2021-12-07 07:11:33 -0500

    solutions for day 1

--- 2021/day01/lib/day01.ex
+++ 2021/day01/lib/day01.ex
@@ -16,10 +16,6 @@ defmodule Day01 do
     :world
   end
 
-  def countIncreased([_head], count) do
-    count
-  end
-
   def countIncreased([head, next | rest]) do
     case head < next do
       true -> countIncreased([next | rest], 1)
@@ -32,6 +28,43 @@ defmodule Day01 do
       true -> countIncreased([next | rest], 1 + count)
       false -> countIncreased([next | rest], count)
     end
+  end
+
+  def countIncreased([_head], count) do
+    count
+  end
+
+  def create_sliding_scale(depths, sliding_sums \\ []) do
+    next_sliding = depths
+      |> Enum.take(3)
+      |> Enum.reduce(0, fn x, acc -> acc + x end)
+    sliding_sums = Enum.concat(sliding_sums, [next_sliding])
+    [_head | rest] = depths
+    case length(rest) > 2 do
+      true -> create_sliding_scale(rest, sliding_sums)
+      false -> sliding_sums
+    end
+  end
+
+  def stream_file(filepath) do
+    File.stream!(filepath)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.to_list
+  end
+
+  def solveInput(filepath) do
+    filepath
+    |> stream_file
+    |> countIncreased
+  end
+
+  def solve_sliding_sums_input(filepath) do
+    filepath
+    |> stream_file
+    |> IO.inspect
+    |> create_sliding_scale
+    |> countIncreased
   end
 
 end
